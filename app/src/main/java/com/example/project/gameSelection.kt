7f72e917513a67107587d706dbd669eb187c575d
haniaht <haniah2016@gmail.com>
2020-04-28 14:48:20 +0300

    project

---
+++ app/src/main/java/com/example/project/gameSelection.kt
@@ -0,0 +1,12 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gameSelection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_selection)
+    }
+}
